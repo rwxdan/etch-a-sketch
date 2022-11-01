0fab53b539657eace058dc99688377d2d8c16e68
rwxdan <dan1elcastro@proton.me>
2022-11-01 14:33:29 -0400

    Create etch a sketch grid Add author to page end

--- main.js
+++ main.js
@@ -1,17 +1,55 @@
-let textBox = document.querySelector(".text-box");
-function show(value) {
-	textBox.value = value;
-}
-
+const rows = document.querySelector(".rows");
+const gridContainer = document.querySelector(".grid-container");
 const dropdown = document.querySelector(".dropdown");
 const options = document.querySelector(".options");
+
+const etchASketch = () => {
+	const childDiv = document.createElement("div");
+	childDiv.classList.add("square");
+	childDiv.addEventListener("mouseover", () => {
+		childDiv.classList.replace("square", "colorized");
+	});
+	gridContainer.appendChild(childDiv);
+};
+
+show = (value) => {
+	rows.value = value;
+	gridContainer.innerHTML = "";
+	gridContainer.style.setProperty(
+		"grid-template-columns",
+		`repeat(${rows.value}, 2fr)`
+	);
+	gridContainer.style.setProperty(
+		"grid-template-rows",
+		`repeat(${rows.value}, 2fr)`
+	);
+	rowsValue = rows.value * rows.value;
+	for (let i = 0; i < rowsValue; i++) {
+		etchASketch();
+	}
+};
+
 dropdown.onclick = () => {
 	options.classList.toggle("active");
 };
 
-const reset = () => {
-	if (textBox.value) {
-		textBox.value = null;
+createGrid = () => {
+	rows.value = null;
+	for (let i = 0; i < 256; i++) {
+		const childDiv = document.createElement("div");
+		childDiv.classList.add("square");
+		gridContainer.appendChild(childDiv);
 	}
-	return;
 };
+
+reset = () => {
+	if (rows.value) {
+		rows.value = null;
+		gridContainer.innerHTML = null;
+		gridContainer.style.setProperty("grid-template-columns", "repeat(16, 2fr)");
+		gridContainer.style.setProperty("grid-template-rows", "repeat(16, 2fr)");
+		createGrid();
+	}
+};
+
+createGrid();
